1414f4a28e4d98f269f63fb8f60f29b147fafe00
czt1126 <18819437904@163.com>
2020-01-14 15:54:12 +0800

    修改readme

--- wx-webserver/app.js
+++ wx-webserver/app.js
@@ -3,14 +3,14 @@ const bodyParser = require("koa-bodyparser");   //处理post请求
 const templating = require('./templating');     //处理跳转页面模板
 const routerController = require('./router');     //加载路由处理器
 
-var app = new Koa();
-app.use(bodyParser());
+var app = new Koa();    //创建Koa实例
+app.use(bodyParser());  //处理表单post请求
 
-// add nunjucks as view:
+// add nunjucks as view: 使用nunjucks
 app.use(templating('views', { 
 }));
 
-app.use(routerController().routes());   
+app.use(routerController().routes());   //注册路由   
 
 app.use(async (ctx) => { 
     console.log("404 Not Found")
